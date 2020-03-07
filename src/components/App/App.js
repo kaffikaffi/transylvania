f04e812b10e513624f9f54350f3b08ec2ee71f6b
Børge S. Dreyer <borge.dreyer@gmail.com>
2020-03-07 20:20:08 +0100

    halla test

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="main-header">SATURDAY IS FOR THE BOIS</h1>
+    </div>
+  );
+};
+export default App;

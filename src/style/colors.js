10542b8051652c3ecc0fb427c4d315fbf12fd277
Børge S. Dreyer <borge.dreyer@gmail.com>
2020-03-08 17:59:09 +0100

    fix export and style

--- src/style/colors.js
+++ src/style/colors.js
@@ -1,4 +1,6 @@
 const pink = "#e80459";
 const purple = "#33044a";
 const blue = "#2fbad1";
-const light_blue = "#d7f3f7";
+const lightBlue = "#d7f3f7";
+
+export default { pink, purple, blue, lightBlue };
